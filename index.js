4a5cf983cbaa33c497605678f8008d64a821141d
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-30 10:25:09 +0700

    feat: add handle route for check status and health for discovery server eureka

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const activeNotificationServiceConsumer = require('./kafka/consumer');
 const router = require('./routes');
 const notificationService = require('./services/notificationService');
 const getAuthAndPutCurrentUserAuthToBody = require('./utils/middlewares/getAuthAndPutCurrentUserAuthToBody');
+const connectToDiscoveryServer = require('./utils/configs/discovery')
 
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -19,9 +20,12 @@ activeNotificationServiceConsumer();
 
 socketBuilder(io);
 
+
 app.use(express.json());
 app.use(getAuthAndPutCurrentUserAuthToBody);
 notificationService.setSocket(io);
-app.use('/api/v1/notifications', router);
+app.use(process.env.APP_PATH || '/api/v1/notifications', router);
+
+connectToDiscoveryServer();
 
 httpServer.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const mapNormalUserRoute = require('./mapNormalUserRoute');
+const mapHealthStatusRoute = require('../utils/eureka/healthStatusRoute');
 
+mapHealthStatusRoute(router);
 mapNormalUserRoute(router);
 
 module.exports = router;
