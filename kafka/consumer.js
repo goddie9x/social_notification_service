afff7bcadc4c00760df9006992ea5fcd227c3444
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-17 16:34:22 +0700

    fix: make all communication with kafka async

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -3,7 +3,7 @@ const notificationService = require('../services/notificationService');
 const { KAFKA_TOPICS } = require('../utils/constants/kafka');
 
 const activeNotificationServiceConsumer = async () => {
-    await createTopicIfNotExists([{ topic: KAFKA_TOPICS.NOTIFICATION_TOPIC.REQUEST }]);
+    await createTopicIfNotExists([{ topic: KAFKA_TOPICS.NOTIFICATION_TOPIC.REQUEST, waitForLeaders: false, numPartitions: 1, replicationFactor: 1 }]);
     activeServiceConsumer({
         serviceInstance: notificationService,
         topic: KAFKA_TOPICS.NOTIFICATION_TOPIC.REQUEST
--- kafka/producer.js
+++ kafka/producer.js
@@ -1,8 +1,8 @@
-const { sendNewSocketMessageToSocketGateway } = require('../utils/kafka/producer');
+const { sendNewMessageToSocketGateway } = require('../utils/kafka/producer');
 const { NOTIFICATION_CHANNEL } = require('../utils/constants/socketChannel');
 
-const pushNewNotificationToSocketGateway = ({ message, roomId }) => {
-    sendNewSocketMessageToSocketGateway({
+const pushNewNotificationToSocketGateway = async ({ message, roomId }) => {
+    await sendNewMessageToSocketGateway({
         namespace: NOTIFICATION_CHANNEL.NAMESPACE,
         event: NOTIFICATION_CHANNEL.EVENTS.NEW_NOTIFICATION,
         message,
